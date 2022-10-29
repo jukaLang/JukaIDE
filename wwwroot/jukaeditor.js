347597a0822ae63d7df8a146e8fa2770cc45dcb6
ALCFAndrey <andreim@jukalang.com>
2022-10-28 22:03:45 -0400

    Download, Upload, GetLink

--- wwwroot/jukaeditor.js
+++ wwwroot/jukaeditor.js
@@ -91,6 +91,18 @@ window.blazorMonaco.languages = {
                 ],
             }
         });
+    },
+    download: function (text) {
+        var element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+        element.setAttribute('download', "main.juk");
+
+        element.style.display = 'none';
+        document.body.appendChild(element);
+
+        element.click();
+
+        document.body.removeChild(element);
     }
 }
 
